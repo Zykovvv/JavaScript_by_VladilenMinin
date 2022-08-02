cd377b0a85a867bad6cd92ff15e84e8e3c765250
Maxim Zykov <zykovworks@yandex.ru>
2022-08-02 09:54:35 +0300

    Изучение строк

---
+++ 03_String.js
@@ -0,0 +1,65 @@
+// const name = "Максим";
+// const age = 25;
+
+// function getAge(){
+//     return age
+// };
+
+
+// const output = "Привет, меня зовут " + name + " и мне: " + age + "лет";
+// const output = `Привет, меня зовут ${name} и мне: ${age} лет`;
+
+// console.log(output)
+
+// const output = `
+//     <div>This is div</div>
+//     <p>this is p</p>
+// `;
+// console.log (output);
+
+
+// const name = "Максим";
+// console.log (name.length);
+// console.log (name.toUpperCase());
+// console.log (name.charAt(2));
+// console.log (name.indexOf());
+// console.log (name.startsWith("Мак"));
+// console.log (name.endsWith("им!"));
+// console.log (name.repeat(3));
+
+// const string = "    password    ";
+// console.log (string.trim());
+// console.log (string.trimLeft());
+// console.log (string.trimRight());
+
+// function logPerson(s, name, age){
+//     if (age < 0){
+//         age = "Еще не родился"
+//     }
+//     return `${s[0]}${name}${s[1]}${age}${s[2]}`;
+// };
+// const personName = "Максим";
+// const personName2 = "Максибом";
+// const personAge = 25;
+// const personAge2 = -10;
+
+// const output = logPerson`Имя: ${personName}, Возраст: ${personAge}`;
+// const output2 = logPerson`Имя: ${personName2}, Возраст: ${personAge2}`;
+
+// console.log(output);
+// console.log(output2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
