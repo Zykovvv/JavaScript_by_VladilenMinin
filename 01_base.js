11b2dc32cdfb0e301b3abec52b029f286a1f8722
Maxim Zykov <zykovworks@yandex.ru>
2022-08-02 09:02:20 +0300

    Добавлены два файла по js,где изучены база и числа

---
+++ 01_base.js
@@ -0,0 +1,286 @@
+// 1 Переменные
+// variable
+// const firstName = "Maxim";
+// const lastName = "Zykov";
+// // let age = 25;
+// const isProgrammer = true; //boolean
+
+// const _private = "private";
+// const $ = "some value";
+
+// // const if = "mkeff" - error
+
+// const withNum5 = "5";
+
+
+// 2 Мутирование
+
+// console.log ("Имя человека:" + firstName + "возраст человека"+ age);
+// alert ("Имя человека:" + firstName + "возраст человека"+ age)
+
+// 3 Операторы
+
+// const a = 10;
+// const b = 5;
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(++currentYear);
+// console.log(--currentYear);
+// console.log (c)
+
+
+// 4 Типы данных
+
+// const isProgrammer = true;
+// const name= "maxim";
+// const age=25;
+
+// let x
+// console.log (typeof isProgrammer);
+// console.log (typeof name);
+// console.log (typeof age);
+// console.log (typeof x);
+// console.log (null);
+
+// 5 Приоритет операторов
+
+// const fullAge = 25;
+// const birthYear = 1997;
+// const currentYear = 2022;
+
+// // > < >= <=
+
+// const isFullAge = currentYear - birthYear >= fullAge // 26 >= 27 true
+// console.log (isFullAge);
+
+
+// 6 Условные операторы
+
+// const courseStatus = "pending";
+
+//  if (courseStatus === "ready") {
+//     console.log("Курс готов к прохождению")
+//  }
+
+//  if (courseStatus === "pending") {
+//     console.log("Курс в процессе разработки")
+//  }
+
+// const isReady = false;
+
+// if (isReady){
+//     console.log ("Все готово");
+// }else {
+//     console.log ("Все не готово")
+// };
+
+// // тернарное выражение
+
+// isReady ? console.log ("Все готово") : console.log ("Все не готово");
+
+
+// 7 Булевая логика
+
+// 8 Функции
+
+// function calculateAge (year){
+//     return 2022 - year
+// }
+
+// // console.log (calculateAge (1997))
+// // console.log (calculateAge (2019))
+// // console.log (calculateAge (1999))
+
+
+// function logInfoAbout (name, year) {
+//     const age = calculateAge(year)
+// if (age > 0){
+//     console.log ("Человек по имени " +name+ "сейчас имеет возраст "+age);
+// }else {
+//     console.log ("ВОообще- то это уже будущее");
+// }
+// };
+
+
+// logInfoAbout ("Maxim", 1997)
+// logInfoAbout ("Kate", 1998)
+// logInfoAbout ("Valery", 2022)
+
+
+// 9 Массивы 
+// const cars = ["Mazda", "Mercedes-Benz", "Land Rover"];
+// console.log (cars.length)
+
+
+
+// 10 Циклы
+
+// const cars = ["Mazda", "Mercedes-Benz", "Land Rover"];
+
+// for (let i = 0; i < cars.length; i++) {
+//     const car = cars [i]
+//     console.log (car)
+// }
+
+// 11 Объекты 
+
+// const person = {
+//     firstName: "Maxim",
+//     lastName: "Zykov",
+//     year: 1997,
+//     languages: ["Ru"],
+//     hasWife: true,
+//     greet: function (){
+//         console.log ("greet from person")
+//     }
+// }
+// conconsole.log(person.firstName);
+// console.log(person.lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
